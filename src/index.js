2d6aa89672660e2227eed7318b1f4203e6f89a7b
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-05-04 15:54:35 +0500

    Modified code with CRUD functionality

--- src/index.js
+++ src/index.js
@@ -1,62 +1,96 @@
 import './styles.css';
-// Grab the elements from the HTML document
-const inputTask = document.querySelector('#new-task');
-const todoList = document.querySelector('#todo-list');
-const addBtn = document.querySelector('.btn');
-const alertElement = document.querySelector('.alert');
 
-// Save the tasks in the local storage
-const saveTask = () => {
-  localStorage.setItem('tasks', todoList.innerHTML);
-};
+// Retrieve the tasks array from local storage
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-// Add a new task to the list
-const addTask = () => {
-  if (inputTask.value === '') {
-    alertElement.style.display = 'flex';
-    saveTask();
-  } else {
-    const li = document.createElement('li');
-    li.textContent = inputTask.value;
-    todoList.appendChild(li);
-    const span = document.createElement('span');
-    span.textContent = '\u00d7';
-    li.appendChild(span);
-    alertElement.style.display = 'none';
-    saveTask();
+// Get DOM elements
+const newTaskInput = document.getElementById('new-task');
+const todoList = document.getElementById('todo-list');
+
+// Function to save tasks to local storage
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Function to add a new task
+function addTask(description) {
+  const task = {
+    description,
+    completed: false,
+    index: tasks.length,
+  };
+  tasks.push(task);
+  saveTasks();
+}
+
+// Function to delete a task
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  // Update indexes of remaining tasks
+  for (let i = index; i < tasks.length; i += 1) {
+    tasks[i].index = i;
   }
-  inputTask.value = '';
-};
+  saveTasks();
+}
 
-// Toggle the "checked" class when clicking on a task
-const toggleChecked = (e) => {
-  if (e.target.tagName === 'LI') {
-    e.target.classList.toggle('checked');
-    saveTask();
+// Function to edit a task's description
+function editTask(index, newDescription) {
+  tasks[index].description = newDescription;
+  saveTasks();
+}
+
+// Populate the To Do List with the existing tasks
+function populateList() {
+  todoList.innerHTML = '';
+  tasks.forEach((task) => {
+    const taskElement = document.createElement('li');
+    taskElement.innerHTML = `
+      <input type="checkbox" ${task.completed ? 'checked' : ''}>
+      <span>${task.description}</span>
+      <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+      <button class="delete"><i class="fa-solid fa-trash"></i></button>
+    `;
+    const checkbox = taskElement.querySelector('input[type="checkbox"]');
+    checkbox.addEventListener('change', () => {
+      task.completed = !task.completed;
+      saveTasks();
+    });
+    const deleteButton = taskElement.querySelector('.delete');
+    deleteButton.addEventListener('click', () => {
+      deleteTask(task.index);
+      populateList();
+    });
+    const editButton = taskElement.querySelector('.edit');
+    editButton.addEventListener('click', () => {
+      const newDescription = prompt('Enter a new description:', task.description);
+      if (newDescription) {
+        editTask(task.index, newDescription);
+        populateList();
+      }
+    });
+    todoList.appendChild(taskElement);
+  });
+}
+
+// Add event listener to the "Add" button
+document.querySelector('.add-task .btn').addEventListener('click', () => {
+  const description = newTaskInput.value.trim();
+  if (description) {
+    addTask(description);
+    populateList();
+    newTaskInput.value = '';
   }
-};
+});
 
-// Remove a task when clicking on the delete button
-const removeTask = (e) => {
-  if (e.target.tagName === 'SPAN') {
-    e.target.parentElement.remove();
-    saveTask();
-  }
-};
+// Add event listener to the "Clear all completed" button
+document.querySelector('button[type="reset"]').addEventListener('click', () => {
+  tasks = tasks.filter((task) => !task.completed);
+  tasks.forEach((task, index) => {
+    task.index = index;
+  });
+  saveTasks();
+  populateList();
+});
 
-// Show the tasks saved in the local storage
-const showTasks = () => {
-  try {
-    todoList.innerHTML = localStorage.getItem('tasks');
-  } catch (e) {
-    console.log('Error getting tasks from local storage:', e);
-  }
-};
-
-// Add event listeners
-addBtn.addEventListener('click', addTask);
-todoList.addEventListener('click', toggleChecked);
-todoList.addEventListener('click', removeTask);
-
-// Show the tasks when the page loads
-showTasks();
+// Populate the list on page load
+populateList();
